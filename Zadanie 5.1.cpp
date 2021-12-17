9374a1728d8ef3e74d53e6b51d29f4b4fcac1504
s25672-pj <92215054+s25672-pj@users.noreply.github.com>
2021-12-17 17:27:30 +0100

    Add files via upload

---
+++ Zadanie 5.1.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    int ilosc;
+    int wartosc;
+    cout << "Podaj liczbe elementow wektora: ";
+    cin >> ilosc;
+    vector<int>vec;
+    for (auto i = 0; i < ilosc; i++)
+    {
+        cout << "Wpisz wartosc elementu wektora[" << i << "]:";
+        cin >> wartosc;
+        vec.push_back(wartosc);
+        
+    }
+    int x = 0;
+    int y;
+    for (auto i = 0; i < ilosc; i++)
+    {
+        if (vec[i] > x)
+        {
+            x = vec[i];
+            y = i;
+        }
+    }
+    std::cout << "Maksymalny element wektora to wektor[" << y << "] i jego wartosc to: " << x;
+}
+
+
+
